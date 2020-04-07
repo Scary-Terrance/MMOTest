f820646134b8b8506a35bf1ba192c2b4e7a27ae9
Ben <oeslian@gmail.com>
2020-04-07 00:24:06 -0400

    Added Basic State Machine, Attack Animations, and a rebuild of the server to ws-rs

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -4,7 +4,21 @@ extends KinematicBody2D
 const ACCELERATION = 500
 const MAX_SPEED = 80
 const FRICTION = 500
-const NETFPS = 0.1
+const NETFPS = 0.05
+
+# State Machine
+enum {
+	PASSIVE,
+	ATTACK
+}
+
+# Class Variables
+var _client = WebSocketClient.new()
+var velocity = Vector2.ZERO
+var netTime = NETFPS
+var state = PASSIVE
+var netState = PASSIVE
+var lastPacket = {}
 
 # Onready Variables
 onready var amimationPlayer = $AnimationPlayer
@@ -14,12 +28,7 @@ onready var animationState = animationTree.get("parameters/playback")
 # Exports
 # "ws://echo.websocket.org"
 export var websocket_url = "ws://127.0.0.1:9001" 
-
-# Class Variables
-var _client = WebSocketClient.new()
-var velocity = Vector2.ZERO
-var netTime = NETFPS
-
+	
 # Runs once on entity initialization
 func _ready():
 	# Connect base signals to get notified of connection open, close, and errors.
@@ -37,13 +46,15 @@ func _ready():
 		print("Unable to connect")
 		set_physics_process(false)
 
+# Runs if the connection closes
 func _closed(was_clean = false):
 	# was_clean will tell you if the disconnection was correctly notified
 	# by the remote peer before closing the socket.
 	print("Closed, clean: ", was_clean)
-	set_process(false)
+	set_physics_process(false)
 
-func _connected(proto = ""):
+# Runs once when connected
+func _connected(_proto = ""):
 	# You MUST always use get_peer(1).put_packet to send data to server,
 	# and not put_packet directly when not using the MultiplayerAPI.
 	_send_packet(Vector2(0.0, 0.0))
@@ -54,36 +65,44 @@ func _on_data():
 	# using the MultiplayerAPI.
 	print("Got data from server: ", _client.get_peer(1).get_packet().get_string_from_utf8())
 
+func _update_last_packet(input_vector):
+	lastPacket["i"] = input_vector
+	lastPacket["v"] = velocity
+	lastPacket["p"] = self.position
+	lastPacket["s"] = netState
+
+func _prep_packet(input_vector):
+	var packet = ""
+	if lastPacket.empty():
+		_update_last_packet(input_vector)
+		packet += "i%.2f %.2f " % [input_vector.x, input_vector.y]
+		packet += "v%.2f %.2f " % [velocity.x, velocity.y]
+		packet += "p%.2f %.2f " % [self.position.x, self.position.y]
+		packet += "s%d " % [netState]
+	if lastPacket["i"] != input_vector:
+		packet += "i%.2f %.2f " % [input_vector.x, input_vector.y]
+	if lastPacket["v"] != velocity:
+		packet += "v%.2f %.2f " % [velocity.x, velocity.y]
+	if lastPacket["p"] != self.position:
+		packet += "p%.2f %.2f " % [self.position.x, self.position.y]
+	if lastPacket["s"] != netState:
+		packet += "s%d " % [netState]
+	_update_last_packet(input_vector)
+	if netState != PASSIVE:
+		netState = PASSIVE
+	return packet
+	
 func _send_packet(input_vector):
 	# Structure the packet
-	var packet = "i%.2f %.2f v%.2f %.2f" % [input_vector.x, input_vector.y, velocity.x, velocity.y]
-	packet += " p%.2f %.2f" % [self.position.x, self.position.y]
+	var packet = _prep_packet(input_vector)
 	# Put the packet in the queu to send
-	_client.get_peer(1).put_packet(packet.to_utf8())
-	
-# Runs every physics tick
-func _physics_process(delta):	
-	# Calculate input_vector from user inputs
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-	# Normalize the vector to prevent doubled speed at 45 degrees
-	input_vector = input_vector.normalized()
-	
-	if input_vector != Vector2.ZERO:
-		# Set the animation blend position to input vector so animations play in right direction
-		animationTree.set("parameters/Idle/blend_position", input_vector)
-		animationTree.set("parameters/Run/blend_position", input_vector)
-		# Since moving Run
-		animationState.travel("Run")
-		# Calculate velocity for motion
-		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
-	else:
-		# Standing still so idle and apply friction
-		animationState.travel("Idle")
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-	
-	velocity = move_and_slide(velocity)
+	if packet != "":
+		_client.get_peer(1).put_packet(packet.to_utf8())
+
+func _poll_net(delta, input_vector):
+#	if !_client.get_peer(1).is_connected_to_host():
+#		print("Error: Connection to Server Closed")
+#		set_physics_process(false)
 	netTime -= delta
 	if netTime <= 0:
 		netTime = NETFPS
@@ -92,6 +111,57 @@ func _physics_process(delta):
 	
 	# Send / Read packets with the server
 	_client.poll()
+	
+func _get_input_vector():
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	# Normalize the vector to prevent doubled speed at 45 degrees
+	return input_vector.normalized()
+	
+func _move(delta, input_vector):
+	if input_vector != Vector2.ZERO:
+		# Set the animation blend position to input vector so animations play in right direction
+		animationTree.set("parameters/idle/blend_position", input_vector)
+		animationTree.set("parameters/run/blend_position", input_vector)
+		if animationState.get_current_node() != "attack":
+			animationTree.set("parameters/attack/blend_position", input_vector)
+		# Since moving Run
+		animationState.travel("run")
+		# Calculate velocity for motion
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		# Standing still so idle and apply friction
+		animationState.travel("idle")
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+func passive_state():
+	if Input.is_action_just_pressed("ui_attack"):
+		state = ATTACK
+		netState = state
+	
+func attack_state():
+	animationState.travel("attack")
+	state = PASSIVE
+	
+func _handle_state():
+	match state:
+		ATTACK:
+			attack_state()
+		PASSIVE:
+			passive_state()
+			
+# Runs every physics tick
+func _physics_process(delta):	
+	# Calculate input_vector from user inputs
+	var input_vector = _get_input_vector()
+	# Calculate current state for State Machine and Animation Tree
+	_move(delta, input_vector)
+	_handle_state()
+	# Calculate current position
+	velocity = move_and_slide(velocity)
+	# Poll the server
+	_poll_net(delta, input_vector)
 
 # Called when entity despawned
 func _exit_tree():
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,38 +1,67 @@
 // Struct for users
-struct Player {
-    username: String,
-    email: String,
-    pos: f64,
-    vel: f64,
-    inp: f64,
+extern crate ws;
+
+use std::rc::Rc;
+use std::cell::Cell;
+
+use ws::{listen, Handler, Sender, Result, Message, Handshake, CloseCode, Error};
+
+struct Server {
+    out: Sender,
+    count: Rc<Cell<u32>>,
+    id: u32,
 }
 
-fn main() {
-    use std::net::TcpListener;
-    use std::thread::spawn;
-    use tungstenite::server::accept;
+struct Player {
+    inp: f32,
+    vel: f32,
+    pos: f32,
+    state: i8,
+    name: String,
+}
 
-    let url = "127.0.0.1:9001";
+impl Handler for Server {
 
-    // A WebSocket echo server
-    let server = TcpListener::bind(url).unwrap();
-    println!("Listening at: {}", url);
-    for stream in server.incoming() {
-        spawn (move || {
-            let mut websocket = accept(stream.unwrap()).unwrap();
-            //println!("{}", websocket);
-            loop {
-                let msg = websocket.read_message().unwrap();
-                println!("{}", msg);
-                // We do not want to send back ping/pong messages.
-                if msg.is_binary() || msg.is_text() {
-                    websocket.write_message(msg).unwrap();
-                }
-                else if msg.is_close() {
-                    println!("Connection closed by user");
-                    break;
-                }
-            }
-        });
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        // We have a new connection, so we increment the connection counter
+        Ok(self.count.set(self.count.get() + 1))
     }
+
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        // Tell the user the current count
+        println!("Connection ID {}: {}", self.id, msg);
+
+        // Echo the message back
+        self.out.send(msg)
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away   => println!("The client is leaving the site."),
+            CloseCode::Abnormal => println!(
+                "Closing handshake failed! Unable to obtain closing status from client."),
+            _ => println!("The client encountered an error: {}", reason),
+        }
+
+        // The connection is going down, so we need to decrement the count
+        self.count.set(self.count.get() - 1)
+    }
+
+    fn on_error(&mut self, err: Error) {
+        println!("The server encountered an error: {:?}", err);
+    }
+
+}
+
+
+fn main() {
+    // Cell gives us interior mutability so we can increment
+    // or decrement the count between handlers.
+    // Rc is a reference-counted box for sharing the count between handlers
+    // since each handler needs to own its contents.
+    let count = Rc::new(Cell::new(0));
+    let url = "127.0.0.1:9001";
+    println!("Server Listening on port: {}", url);
+    listen(url, |out| { Server { out: out, count: count.clone(), id: count.get() } }).unwrap();
 }
